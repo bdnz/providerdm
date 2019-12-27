2b9e8599f43972c008a635b6df6466b4a38dec61
Deniz <Deniz@Deniz-MacBook-Pro.local>
2019-12-27 16:11:58 -0700

    Interface, Auth and Fiebase Database

---
+++ TestAuthProvider/MapKit.swift
@@ -0,0 +1,30 @@
+//
+//  MapKit.swift
+//  TestAuthProvider
+//
+//  Created by Deniz on 6/5/19.
+//  Copyright © 2019 Deniz. All rights reserved.
+//
+
+import UIKit
+
+class MapKit: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TestAuthProvider/TextFields.swift
@@ -0,0 +1,12 @@
+//
+//  TextFieldsandMap.swift
+//  TestAuthProvider
+//
+//  Created by Deniz on 9/24/19.
+//  Copyright © 2019 Deniz. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
